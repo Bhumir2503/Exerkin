49f51210325bedcceb58baf1eb102b1879e12675
Bhumir2503 <bhumir2503@gmail.com>
2025-01-24 22:41:17 -0500

    Apple and Google button Added

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { UserProvider, useUser } from "./src/contexts/UserContext";
+
+import auth from "@react-native-firebase/auth";
+
+import AppleAuthButton from "./src/components/AppleAuthButton";
+import GoogleAuthButton from "./src/components/GoogleAuthButton";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<UserProvider>
+			<AppContent />
+		</UserProvider>
+	);
+}
+
+function AppContent() {
+	const { user, init } = useUser();
+
+	if (init) {
+		return null;
+	}
+
+	if (user) {
+		return (
+			<View style={styles.container}>
+				<Text>Welcome {user.displayName}</Text>
+				<StatusBar style="auto" />
+				<Text onPress={() => auth().signOut()}>Signout</Text>
+			</View>
+		);
+	}
+
+	return (
+		<View style={styles.container}>
+			<AppleAuthButton />
+			<GoogleAuthButton />
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
