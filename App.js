b77e0217d58afeff94bc10f1543045324f16cabf
Bhumir2503 <36862986+Bhumir2503@users.noreply.github.com>
2025-01-31 22:05:24 -0500

    Navigation Bar Hide

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { Platform } from "react-native";
+import React, {useEffect} from "react";
 import { MenuProvider } from "react-native-popup-menu";
 import { NavigationContainer } from "@react-navigation/native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
@@ -8,6 +8,7 @@ import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { UserProvider, useUser } from "./src/contexts/UserContext";
 import {ThemeProvider } from "./src/contexts/ThemeContext";
 import { enableScreens } from "react-native-screens";
+import * as NavigationBar from "expo-navigation-bar";
 
 import Onboarding from "./src/pages/Auth/Onboarding";
 import AppNavigator from "./src/navigations/AppNavigator";
@@ -16,6 +17,21 @@ import AppNavigator from "./src/navigations/AppNavigator";
 enableScreens();
 
 export default function App() {
+
+	if (Platform.OS === "android") {
+		NavigationBar.setVisibilityAsync("hidden");
+	}
+
+	useEffect(() => {
+		if (Platform.OS === "android") {
+			const intervalId = setInterval(() => {
+				NavigationBar.setVisibilityAsync("hidden");
+			}, 5000);
+
+			return () => clearInterval(intervalId); // Cleanup on unmount
+		}
+	}, []);
+
 	return (
 		<ThemeProvider>
 			<SafeAreaProvider>
