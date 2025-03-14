1788af1b88391572b4783e634f48de11431cbe9d
Bhumir2503 <bhumir2503@gmail.com>
2025-03-14 13:00:21 -0400

    getting template started

--- src/components/WorkoutPage/WorkoutDashboard.jsx
+++ src/components/WorkoutPage/WorkoutDashboard.jsx
@@ -1,7 +1,15 @@
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import {
+	View,
+	Text,
+	StyleSheet,
+	TouchableOpacity,
+	ScrollView,
+	Pressable,
+} from "react-native";
 import { useTheme } from "../../contexts/ThemeContext";
 import { useWorkout } from "../../contexts/WorkoutContext";
 import { Ionicons } from "@expo/vector-icons";
+import { useState } from "react";
 
 const WorkoutDashboard = ({ onStartWorkout }) => {
 	const { themeStyle } = useTheme();
@@ -11,16 +19,69 @@ const WorkoutDashboard = ({ onStartWorkout }) => {
 	const buttonPress = () => {
 		newWorkoutStarted();
 		onStartWorkout();
-	}
+	};
 
 	return (
 		<View style={styles.container}>
 			<Text style={styles.title}>Get Started</Text>
 			<Text style={styles.description}>Ready to start your workout?</Text>
-			<StartWorkoutButton
-				title="Start Workout"
-				onPress={buttonPress}
-			/>
+			<StartWorkoutButton title="Start Workout" onPress={buttonPress} />
+			<ScrollView
+				bounces={false}
+				showsVerticalScrollIndicator={false}
+				style={{
+					marginTop: 20,
+					height: "100%",
+					padding: 10,
+					paddingHorizontal: 0,
+				}}
+			>
+				<View
+					style={{
+						backgroundColor: themeStyle.card,
+						padding: 10,
+						borderRadius: 10,
+					}}
+				>
+					<Text style={styles.DailyGoalTitle}>Daily Challenge</Text>
+					<DailyGoal goalName="10km Run" />
+					<DailyGoal goalName="100 Push-Ups" />
+					<DailyGoal goalName="100 Sit-Ups" />
+					<DailyGoal goalName="100 Squats" />
+				</View>
+				<View
+					style={{
+						flexDirection: "row",
+						justifyContent: "space-between",
+						alignItems: "center",
+						marginTop: 20,
+					}}
+				>
+					<Text style={styles.TemplateTitle}>Templates </Text>
+
+					<Pressable
+						onPress={() => console.log("Bhumir Rules")}
+						style={{
+							flexDirection: "row",
+							alignItems: "center",
+							padding: 5,
+							paddingHorizontal: 15,
+							backgroundColor: themeStyle.primary,
+							borderRadius: 5,
+						}}
+					>
+						<Text
+							style={{
+								color: themeStyle.textColor,
+								fontSize: 16,
+								fontWeight: "bold",
+							}}
+						>
+							New
+						</Text>
+					</Pressable>
+				</View>
+			</ScrollView>
 		</View>
 	);
 };
@@ -30,9 +91,28 @@ const StartWorkoutButton = ({ title, onPress }) => {
 	const styles = createStyles(themeStyle);
 	return (
 		<TouchableOpacity style={styles.button} onPress={onPress}>
-            <Ionicons name="fitness-sharp" size={24} color={"#fff"} />
+			<Ionicons name="fitness-sharp" size={24} color={"#fff"} />
 			<Text style={styles.buttonText}>{title}</Text>
 		</TouchableOpacity>
+	);
+};
+
+const DailyGoal = ({ goalName }) => {
+	const [completed, setCompleted] = useState(false);
+	const { themeStyle } = useTheme();
+	const styles = createStyles(themeStyle);
+
+	return (
+		<View style={{ flexDirection: "row", alignItems: "center" }}>
+			<Ionicons
+				style={{ marginBottom: 10 }}
+				name={completed ? "checkbox" : "square"}
+				size={24}
+				color={completed ? themeStyle.primary : themeStyle.textColor}
+				onPress={() => setCompleted(!completed)}
+			/>
+			<Text style={styles.DailyGoalText}>{goalName}</Text>
+		</View>
 	);
 };
 
@@ -42,32 +122,52 @@ const createStyles = (theme) => {
 			flex: 1,
 			margin: 20,
 			marginTop: 10,
+			marginBottom: 0,
 		},
 		title: {
 			color: theme.textColor,
 			fontSize: 32,
-            fontWeight: "bold",
+			fontWeight: "bold",
 			marginBottom: 10,
 		},
 		description: {
 			color: theme.textColor,
 			fontSize: 18,
-            marginBottom: 20,
+			marginBottom: 20,
 		},
 		button: {
 			backgroundColor: theme.primary,
 			padding: 10,
 			borderRadius: 5,
-            flexDirection: "row",
-            justifyContent: "center",
-            alignItems: "center",
+			flexDirection: "row",
+			justifyContent: "center",
+			alignItems: "center",
 		},
 		buttonText: {
 			color: "#fff",
-            fontSize: 18,
-            fontWeight: "bold",
+			fontSize: 18,
+			fontWeight: "bold",
 			textAlign: "center",
-            marginLeft: 10,
+			marginLeft: 10,
+		},
+
+		DailyGoalTitle: {
+			color: theme.textColor,
+			fontSize: 18,
+			fontWeight: "bold",
+			marginBottom: 10,
+		},
+		DailyGoalText: {
+			color: theme.textColor,
+			fontSize: 16,
+			marginBottom: 10,
+			marginLeft: 10,
+		},
+		TemplateTitle: {
+			color: theme.textColor,
+			fontSize: 24,
+			fontWeight: "bold",
+			marginBottom: 10,
 		},
 	});
 };
