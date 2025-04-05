20f9ccc7790c53915696a67a525c457034e1a5e9
Bhumir2503 <bhumir2503@gmail.com>
2025-04-04 22:08:11 -0400

    removed useless node modules from package json

---
+++ src/components/BlueprintPage/TemplateModal.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {
+	View,
+	Platform,
+	StyleSheet,
+	StatusBar,
+	SafeAreaView,
+	KeyboardAvoidingView,
+} from "react-native";
+import { useTheme } from "../../contexts/ThemeContext";
